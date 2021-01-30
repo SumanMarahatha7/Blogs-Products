3775808451572bd33a2032a7826fe8325307833b
Suman Marahatha <sumancr.marahatha@gmail.com>
2021-01-30 10:47:32 +0545

    finished working with API

---
+++ backend/BP/products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.CategoryList.as_view()),
+    path('productlist/',views.ProductList.as_view()),
+    path('<int:pk>',views.CategoryDetail.as_view()),
+    path('highlight',views.Show.as_view()),
+]
